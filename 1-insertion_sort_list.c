b742e7d44c0593c0e5a59038c8b1e1179c45cdee
El-Zieny <abdoelzieny72@gmail.com>
2023-09-25 23:25:06 +0300

    select sort algorythim

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list using insertion  algorithm
+ * @list: the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *ahead;
+
+	current = *list;
+	ahead = current->next;
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array by selection sort algorithm
+ * @array: the array to be sorted
+ * @size: size
+ */
+void selection_sort(int *array, size_t size)
+{
+	int z, tmp, cmp;
+	size_t i, x;
+
+	for (x = 0; x < size - 1; x++)
+	{
+		cmp = array[x];
+		z = x;
+		for (i = x + 1; i < size; i++)
+		{
+			if (cmp > array[i])
+			{
+				cmp = array[i];
+				z = i;
+			}
+		}
+		if (array[x] != cmp)
+		{
+			tmp = array[x];
+			array[x] = cmp;
+			array[z] = tmp;
+			print_array(array, size);
+		}
+	}
+}
